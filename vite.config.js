767122e960f64a469adf5728060aaf1f436c3a17
CarterIsrael17 <adetunjiisrael2@gmail.com>
2025-08-13 15:22:09 +0100

    Resolve vite.config.js merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,12 +5,3 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 });
-=======
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
->>>>>>> 6e660a5665e8ba0aeb830d396a266ce5aad78e30
