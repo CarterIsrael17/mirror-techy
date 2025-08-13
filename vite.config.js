58fb38165ae899dedc84524dadb17a9806fcdf3c
CarterIsrael17 <adetunjiisrael2@gmail.com>
2025-08-13 15:42:55 +0100

    Fix SPA routing for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist/client' // change build output folder
-  }
+    outDir: 'dist', // Matches Netlify's publish directory
+  },
 });
